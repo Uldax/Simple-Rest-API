78a01ee941af4cd4b511bb65c25e4ea608a8541e
pcontat <contat.paul@gmail.com>
2015-10-31 14:10:14 -0400

    Auth part 1 : add user model and auth methodes

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,20 +1,27 @@
 var jwt = require('jwt-simple');
+var User = require('../models/user');
+
 var auth = {
+    // Fire a query to your DB and check if the credentials are valid
+    validate: function(username, password) {
+        // spoofing the DB response for simplicity
+       if (username === '' || password === '') {
+            return;
+       }
+       User.findOne( { 'username' : username, 'password':password}, function(err, user) {
+            if (err) {
+                return ;
+            }
+            return user;
+        });
+    },
+
     login: function(req, res) {
         var username = req.body.username || '';
         var password = req.body.password || '';
-        if (username === '' || password === '') {
-            res.status(401);
-            res.json({
-                "status": 401,
-                "message": "Invalid credentials"
-            });
-            return;
-        }
-
-        // Fire a query to your DB and check if the credentials are valid
         var dbUserObj = auth.validate(username, password);
-        if  (!dbUserObj) { // If authentication fails, we send a 401 back
+        // If authentication fails
+        if  (!dbUserObj) {
                 res.status(401);
                 res.json({
                 "status": 401,
@@ -22,31 +29,11 @@ var auth = {
             });
             return;
         }
-        if (dbUserObj) {
+        else {
             // If authentication is success, we will generate a token
             // and dispatch it to the client
             res.json(genToken(dbUserObj));
         }
-    },
-    validate: function(username, password) {
-        // spoofing the DB response for simplicity
-        // TODO finish here
-        var dbUserObj = { // spoofing a userobject from the DB.
-            name: 'arvind',
-            role: 'admin',
-            username: 'arvind@myapp.com'
-        };
-        return dbUserObj;
-    },
-
-    validateUser: function(username) {
-        // spoofing the DB response for simplicity
-        var dbUserObj = { // spoofing a userobject from the DB.
-            name: 'arvind',
-            role: 'admin',
-            username: 'arvind@myapp.com'
-        };
-        return dbUserObj;
     },
 };
 
